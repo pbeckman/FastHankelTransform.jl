38fb68a217be0b589c47e16cfee72ff56e3c75ee
Paul Beckman <paul.beckman@cims.nyu.edu>
2024-10-08 13:17:22 -0400

    NUFFT 2D comparison added

---
+++ manuscript/figures/code/fourier_scaling_fig.jl
@@ -0,0 +1,324 @@
+using LinearAlgebra, FastGaussQuadrature, FINUFFT, FastHankelTransform
+using Printf, Plots, TimerOutputs, BenchmarkTools, LaTeXStrings, JLD
+import Bessels: besselj
+
+include("../../../example/util.jl")
+include("/Users/beckman/.julia/config/custom_colors.jl")
+
+function quad1D(m, a, b)
+    # Gauss-Legendre on [a,b]
+    rs, wts = gausslegendre(m)
+    rs   .= (rs .+ 1) * (b-a)/2 .+ a
+    wts .*= (b-a)/2
+
+    return rs, wts
+end
+
+function quad2D(mgl, mtrs, a, b)
+    # Gauss-Legendre cross trapezoidal on the annulus [a,b]
+    rsgl, wtsgl = quad1D(mgl, a, b)
+
+    rs  = Matrix{Float64}(undef, 2, sum(mtrs))
+    wts = Vector{Float64}(undef, sum(mtrs))
+    i = 1
+    for (j, mtr) in enumerate(mtrs)
+        rs[1,i:(i+mtr-1)] .= rsgl[j]
+        rs[2,i:(i+mtr-1)] .= range(0, stop=2pi, length=mtr+1)[1:end-1]
+        wts[i:(i+mtr-1)]  .= wtsgl[j] * 2pi/mtr
+
+        i += mtr
+    end
+
+    return rs, wts
+end
+
+function hankel_integrate2D(f, rs, wts, ws; tol=1e-8)
+    # use the Hankel transform to compute 2D Fourier transform
+    return 2pi * nufht(
+        0, 
+        rs, f.(rs) .* rs .* wts, 
+        ws, tol=tol
+        )
+end
+
+function fourier_integrate1D(r, mtr, ws; tol=1e-8)
+    # use the 1D Fourier transform to compute marginal Fourier transform 
+    # on a ring of fixed radius r
+    ss = range(0, stop=2pi, length=mtr+1)[1:end-1]
+    return real.(nufft1d3(
+        r*cos.(ss),
+        2pi/mtr * ones(ComplexF64, mtr), 
+        -1, tol,
+        ws
+        ))
+end
+
+function fourier_integrate2D(f, rs, wts, ws; tol=1e-8)
+    return real.(nufft2d3(
+        rs[1,:] .* cos.(rs[2,:]), zeros(size(rs, 2)),
+        Complex.(f.(rs[1,:]) .* rs[1,:] .* wts), 
+        -1, tol,
+        ws, zeros(length(ws))
+        ))
+end
+
+##
+
+# choose tolerance
+tol = 1e-8
+
+# stop NUFFT evaluation before FINUFFT crashes Julia
+max_quad = 10_000_000
+
+# hat function and true Fourier transform
+a, b     = 0, 1
+f(r)     = 1
+f_hat(w) = 2pi * (b * besselj(1, b*w) - a * besselj(1, a*w)) / w
+
+# max frequencies to evaluate
+# w_maxs = 2 .^ (6:3:21)
+w_maxs = 2 .^ (18:3:21)
+
+# number of points to evaluate
+ns = round.(Int64, 10 .^ range(3, stop=7, length=8))
+
+ntest = 1000
+rs1, wts1 = [], []
+m1s = zeros(Int64, length(w_maxs))
+m2s = zeros(Int64, length(w_maxs))
+m2trss = Vector{Vector{Int64}}(undef, length(w_maxs))
+time_1Ds = fill(NaN, length(w_maxs), length(ns))
+time_2Ds = fill(NaN, length(w_maxs), length(ns))
+aborted_1D = false
+aborted_2D = false
+for (j, w_max) in enumerate(w_maxs)
+    # use coarsest grid
+    n  = ns[1]
+    ws = collect(range(0, stop=w_max, length=n+1))[2:end]
+    f_hat_true = f_hat.(ws)
+
+    if !aborted_1D
+        # adaptively determine necessary quadrature
+        m1 = 8
+        converged = false
+        f_hat_1D  = zeros(Float64, n)
+        while !converged
+            m1 *= 2
+            rs1, wts1 = quad1D(m1, a, b)
+            if m1 > max_quad 
+                @warn "1D quadrature with $m1 > $max_quad = max_quad nodes!"
+                aborted_1D = true
+                break
+            end
+            f_hat_1D .= hankel_integrate2D(f, rs1, wts1, ws, tol=tol)
+            rel_err   = norm(f_hat_true .- f_hat_1D) / norm(f_hat_true)
+            @printf("1D quadrature with %i nodes gives relative error %.2e\n", m1, rel_err)
+            converged = (rel_err < tol*100)
+        end
+        m1s[j] = m1
+    end
+
+    if !aborted_2D
+        m2gl = m1
+        m2trs = Vector{Int64}(undef, m2gl)
+        f_hat_2D = zeros(Float64, n)
+        for k=1:m2gl
+            if k == 1 || mod(k, ceil(Int64, m2gl / ntest)) == 0 
+                m2tr = k == 1 ? 4 : div(m2trs[k-1],2)
+                converged  = false
+                f_hat_ref  = fourier_integrate1D(rs1[k], m2tr, ws; tol=tol)
+                while !converged
+                    m2tr *= 2
+                    if m2tr > max_quad 
+                        @warn "marginal trapezoidal quadrature with $m2tr > $max_quad = max_quad nodes!"
+                        aborted_2D = true
+                        break
+                    end
+                    f_hat_circle = fourier_integrate1D(rs1[k], m2tr, ws; tol=tol)
+                    rel_err = norm(f_hat_ref .- f_hat_circle) / norm(f_hat_ref)
+                    f_hat_ref .= f_hat_circle
+                    @printf("marginal trapezoidal quadrature with %i nodes on circle of radius %.3f gives relative error %.2e\n", m2tr, rs1[k], rel_err)
+                    converged = (rel_err < tol*100)
+                end
+                m2trs[k] = m2tr
+            else
+                m2trs[k] = m2trs[k-1]
+            end
+        end
+        if sum(m2trs) > max_quad 
+            @warn "2D quadrature with $(sum(m2trs)) > $max_quad = max_quad nodes!"
+            aborted_2D = true
+        else
+            rs2, wts2 = quad2D(m2gl, m2trs, a, b)
+            f_hat_2D = fourier_integrate2D(f, rs2, wts2, ws, tol=tol)
+            rel_err  = norm(f_hat_true .- f_hat_2D) / norm(f_hat_true)
+            @printf("2D quadrature with %i nodes gives relative error %.2e\n", sum(m2trs), rel_err)
+        end
+        m2s[j]    = sum(m2trs)
+        m2trss[j] = m2trs
+    end
+    
+    for (i, n) in enumerate(ns)
+        ws = collect(range(0, stop=w_max, length=n+1))[2:end]
+
+        @printf("
+==============================================================
+w_max = %i (%i of %i), n = %i (%i of %i), tol = %.1e
+==============================================================\n", 
+        w_max, j, length(w_maxs), n, i, length(ns), tol
+        )
+
+        if !aborted_1D
+            time_1Ds[j, i] = @belapsed begin 
+                hankel_integrate2D($f, $rs1, $wts1, $ws, tol=$tol)
+            end
+        end
+
+        save(
+            "./fourier_scaling.jld", 
+            "w_maxs", w_maxs,
+            "ns", ns,
+            "time_1Ds", time_1Ds,
+            "time_2Ds", time_2Ds
+        )
+
+        @printf("
+--------------------------------------------------------------
+n = %i, tol = %.1e, w_max = %.2e
+1D quadrature with %i nodes: %.2e sec
+--------------------------------------------------------------\n", 
+        n, tol, w_max, m1, time_1Ds[j, i]
+        )
+
+        if !aborted_2D
+            time_2Ds[j, i] = @belapsed begin 
+                fourier_integrate2D($f, $rs2, $wts2, $ws, tol=$tol)
+            end
+        end
+
+        save(
+            "./fourier_scaling.jld", 
+            "w_maxs", w_maxs,
+            "ns", ns,
+            "time_1Ds", time_1Ds,
+            "time_2Ds", time_2Ds
+        )
+
+        @printf("
+--------------------------------------------------------------
+n = %i, tol = %.1e, w_max = %.2e
+2D quadrature with %i nodes: %.2e sec
+--------------------------------------------------------------\n", 
+        n, tol, w_max, sum(m2trs), time_2Ds[j, i]
+        )
+    end
+end
+
+## Plot example 2D quadrature
+
+gr(size=(300,300))
+default(fontfamily="Computer Modern")
+Plots.scalefontsizes()
+Plots.scalefontsizes(1.25)
+
+j = 1
+rs2, _ = quad2D(m1s[j], m2trss[j], a, b)
+rs1, _ = quad1D(m1s[j], a, b)
+
+nplot = 300
+ts = range(-b, b, nplot)
+# heatmap(ts, ts, ones(nplot,1) * cos.(w_maxs[j]*ts'), color=:acton, alpha=0.2, legend=:none)
+pl = scatter(
+    rs2[1,:].*cos.(rs2[2,:]), rs2[1,:].*sin.(rs2[2,:]), 
+    marker=(1.2, :black), label="",
+    xlabel=L"$x_1$", ylabel=L"$x_2$"
+    )
+scatter!(pl, 
+rs1, zeros(m1s[j]), 
+    marker=(2.5, scrungle[10]), label="",
+    markerstrokewidth=1, markerstrokecolor=:white
+    )
+
+display(pl)
+savefig(pl, "~/Downloads/quadrature_2D.pdf")
+
+## Plot scaling
+
+if isfile("./fourier_scaling.jld")
+    dict     = load("./fourier_scaling.jld")
+    ns       = dict["ns"]
+    w_maxs   = dict["w_maxs"]
+    time_1Ds = dict["time_1Ds"]
+    time_2Ds = dict["time_2Ds"]
+end
+
+gr(size=(400,300))
+
+pal = scrungle
+
+markers = [:circle, :rect, :utriangle, :cross, :star5, :diamond, :hexagon,  :xcross,  :dtriangle, :rtriangle, :ltriangle, :pentagon, :heptagon, :octagon, :star6, :star7, :star8, :vline, :hline, :+, :x]
+
+pl = plot(
+        xscale=:log10, yscale=:log10,
+        xlabel="n", ylabel="time (s)",
+        labels=["1D Hankel" "2D Fourier"],
+        legend=:topleft, legend_columns=2, 
+        legendtitle=" ", legendtitlefontsize=10,
+        ylims=(1e-3, 2e2),
+        yticks=([1e-2, 1e0, 1e2], [L"10^{-2}", L"10^0", L"10^{2}"]),
+        yformatter=:scientific
+        )
+for j in eachindex(w_maxs)
+    plot!(pl,
+        ns, time_1Ds[j, :], 
+        marker=(4, markers[j]), markerstrokewidth=0, 
+        color=pal[j], line=(2, :solid),
+        label=string(Int64(log2(w_maxs[j])))
+        # label=latexstring(@sprintf("\\log_2\\omega_{max} = %i", log2(w_maxs[j])))
+    )
+    plot!(pl,
+        ns, time_2Ds[j, :], 
+        marker=(4, markers[j]), markerstrokewidth=0, 
+        color=pal[j], line=(2, :dash),
+        label=""
+    )
+end
+display(pl)
+savefig(pl, "~/Downloads/fourier_scaling.pdf")
+
+##
+
+gr(size=(1000, 1000*m1s[end]/n))
+pl = skeleton_plot(rs1, ws)
+display(pl)
+
+gr(size=(500,500))
+
+if m2gl*m2tr < 100_000
+    pl = scatter(
+        rs2[1,:] .* cos.(rs2[2,:]), 
+        rs2[1,:] .* sin.(rs2[2,:]), 
+        marker=(2, :black), xlims=(-b,b), ylims=(-b,b), label=""
+        )
+    display(pl)
+end
+
+sk = max(1, div(n, 1000))
+
+pl = plot(ws[1:sk:end], f_hat_true[1:sk:end], label="true")
+plot!(pl, ws[1:sk:end], f_hat_1D[1:sk:end], label="1D quadrature")
+plot!(pl, ws[1:sk:end], f_hat_2D[1:sk:end], label="2D quadrature")
+display(pl)
+
+pl = plot(yscale=:log10, ylims=(1e-16, 1e0))
+plot!(pl,
+    ws[1:sk:end], 
+    abs.((f_hat_true .- f_hat_1D) ./ f_hat_true)[1:sk:end], 
+    label="1D quadrature"
+    )
+plot!(pl, 
+    ws[1:sk:end], 
+    abs.((f_hat_true .- f_hat_2D) ./ f_hat_true)[1:sk:end], 
+    label="2D quadrature"
+    )
+display(pl)
--- manuscript/figures/code/performance_fig.jl
+++ manuscript/figures/code/performance_fig.jl
@@ -1,4 +1,4 @@
-using FastHankelTransform, FastGaussQuadrature, BenchmarkTools, Plots, Plots.Measures, LaTeXStrings, Printf, DelimitedFiles, Random, LinearAlgebra
+using FastHankelTransform, FastGaussQuadrature, BenchmarkTools, Plots, Plots.Measures, LaTeXStrings, Printf, DelimitedFiles, Random, LinearAlgebra, JLD
 
 include("/Users/beckman/.julia/config/custom_colors.jl")
 
@@ -6,11 +6,6 @@ gr(size=(350,300))
 default(margins=1mm, fontfamily="Computer Modern", label="")
 Plots.scalefontsizes()
 Plots.scalefontsizes(1.25)
-
-ns  = round.(Int64, 10 .^ range(3, 7, 10))
-ps  = round.(Int64, 10 .^ range(5, 9, 10))
-
-ns_both = 2 .^ (8:2:20)
 
 function test_case(case, ns, ps, i; nu=0)
     if case == :exp
@@ -59,100 +54,165 @@ end
 
 ## Test scaling with n, m, and p
 
-n_timings = fill(NaN, 2, length(ns))
-m_timings = fill(NaN, 2, length(ns))
-p_timings = fill(NaN, 2, length(ps))
-for (case, timings) in zip([:n, :m, :p], [n_timings, m_timings, p_timings])
-    for i in axes(timings, 2)
-        rs, cs, ws, n, m, p = test_case(case, ns, ps, i)
+if isfile("./nmp_scaling.jld")
+    jld = load("./nmp_scaling.jld")
+    ns        = jld["ns"]
+    ps        = jld["ps"]
+    n_timings = jld["n_timings"]
+    m_timings = jld["m_timings"]
+    p_timings = jld["p_timings"]
+else
+    ns = round.(Int64, 10 .^ range(3, 8, 10))
+    ps = round.(Int64, 10 .^ range(5, 9, 10))
+    n_timings = fill(NaN, 2, length(ns))
+    m_timings = fill(NaN, 2, length(ns))
+    p_timings = fill(NaN, 2, length(ps))
+    for (case, timings) in zip([:n, :m, :p], [n_timings, m_timings, p_timings])
+        for i in axes(timings, 2)
+            rs, cs, ws, n, m, p = test_case(case, ns, ps, i)
 
-        @printf(
-            "Timing %s scaling with n = %i, m = %i, p = %.1e (%i of %i)\n", 
-            string(case), n, m, p, i, size(timings, 2)
-            )
-            
-        if max(n, m) < 200_000
-            timings[1, i] = @belapsed dir(0, $rs, $cs, $ws)
+            @printf(
+                "Timing %s scaling with n = %i, m = %i, p = %.1e (%i of %i)\n", 
+                string(case), n, m, p, i, size(timings, 2)
+                )
+                
+            if max(n, m) < 2_500_000
+                timings[1, i] = @belapsed dir(0, $rs, $cs, $ws)
+            end
+
+            timings[2, i] = @belapsed nufht(0, $rs, $cs, $ws, tol=1e-8)
         end
-
-        timings[2, i] = @belapsed nufht(0, $rs, $cs, $ws, tol=1e-8)
     end
+    save(
+        "./nmp_scaling.jld", 
+        "ns", ns,
+        "ps", ps,
+        "n_timings", n_timings, 
+        "m_timings", m_timings, 
+        "p_timings", p_timings
+        )
 end
 
 ## Test scaling for scaled roots and exponential
 
-nus  = [0, 10, 20, 50, 100]
-tols = [1e-4, 1e-8, 1e-12, 1e-15]
-roots_timings = fill(NaN, 2, length(nus), length(tols), length(ns_both))
-exp_timings   = fill(NaN, 2, length(ns_both))
-for (case, nu, tol, timings) in [
-        (:exp, 0, 1e-8, exp_timings),
-        [(:roots, 0, tols[j], @view(roots_timings[:,1,j,:])) for j=1:length(tols)]...,
-        [(:roots, nus[j], 1e-8, @view(roots_timings[:,j,2,:])) for j=2:length(nus)]...
-    ]
-    for i in axes(timings, 2)
-        rs, cs, ws, n, m, p = test_case(case, ns_both, nothing, i, nu=nu)
+if isfile("./both_scaling.jld")
+    jld = load("./both_scaling.jld")
+    ns_both       = jld["ns"]
+    nus           = jld["nus"]
+    tols          = jld["tols"]
+    roots_timings = jld["roots_timings"]
+    exp_timings   = jld["exp_timings"]
+else
+    ns_both = 2 .^ (8:2:20)
+    nus  = [0, 10, 20, 50, 100]
+    tols = [1e-4, 1e-8, 1e-12, 1e-15]
+    roots_timings = fill(NaN, 2, length(nus), length(tols), length(ns_both))
+    exp_timings   = fill(NaN, 2, length(ns_both))
+    for (case, nu, tol, timings) in [
+            (:exp, 0, 1e-8, exp_timings),
+            [(:roots, 0, tols[j], @view(roots_timings[:,1,j,:])) for j=1:length(tols)]...,
+            [(:roots, nus[j], 1e-8, @view(roots_timings[:,j,2,:])) for j=2:length(nus)]...
+        ]
+        for i in axes(timings, 2)
+            rs, cs, ws, n, m, p = test_case(case, ns_both, nothing, i, nu=nu)
 
-        @printf(
-            "Timing %s scaling with ν = %i, ε = %.0e, n = %i, p = %.1e (%i of %i)\n", 
-            string(case), nu, tol, n, p, i, size(timings, 2)
-            )
+            @printf(
+                "Timing %s scaling with ν = %i, ε = %.0e, n = %i, p = %.1e (%i of %i)\n", 
+                string(case), nu, tol, n, p, i, size(timings, 2)
+                )
 
-        if max(n, m) < 100_000 && tol == 1e-8
-            timings[1, i] = @belapsed dir($nu, $rs, $cs, $ws)
+            if max(n, m) < 100_000 && tol == 1e-8 && nu in nus[1:2]
+                timings[1, i] = @belapsed dir($nu, $rs, $cs, $ws)
+            end
+
+            FastHankelTransform.setup_nufht!(nu, tol)
+
+            timings[2, i] = @belapsed nufht($nu, $rs, $cs, $ws, tol=$tol)
         end
-
-        FastHankelTransform.setup_nufht!(nu, tol)
-
-        timings[2, i] = @belapsed nufht($nu, $rs, $cs, $ws, tol=$tol)
     end
+    save(
+        "./both_scaling.jld", 
+        "ns", ns_both,
+        "nus", nus,
+        "tols", tols,
+        "roots_timings", roots_timings, 
+        "exp_timings", exp_timings
+        )
 end
 
 ## Test accuracy as a function of tolerance
 
-nu = 0
-ns_acc = 10 .^ [3, 5, 7]
-tols = 10.0 .^ (-4:-1:-15)
-gs_true  = [fill(NaN, n) for n in ns_acc]
-gs_nufht = [fill(NaN, length(tols), n) for n in ns_acc]
+if isfile("./accuracy.jld")
+    jld = load("./accuracy.jld")
+    nu       = jld["nu"]
+    n_trials = jld["n_trials"]
+    tols     = jld["tols"]
+    ns_acc   = jld["ns"]
+    gs_true  = jld["gs_true"]
+    gs_nufht = jld["gs_nufht"]
+else
+    nu = 0
+    n_trials = 1
+    tols = 10.0 .^ (-4:-1:-15)
+    ns_acc = 10 .^ [3, 5, 7]
+    gs_true  = [fill(NaN, n) for n in ns_acc, _=1:n_trials]
+    gs_nufht = [fill(NaN, n) for _ in eachindex(tols), n in ns_acc, _=1:n_trials]
+    for (j, n) in enumerate(ns_acc)
+        for t=1:n_trials
+            # make coefficient vector with small number of nonzeros
+            nnz = minimum(ns_acc)
+            I = Random.randperm(n)[1:nnz]
+            V = randn(nnz)
+            cs = zeros(n)
+            cs[I] .= V
+            rs, _, ws = test_case(:roots, [n], nothing, 1, nu=nu)
 
-for (j, n) in enumerate(ns_acc)
-    # make coefficient vector with small number of nonzeros
-    nnz = minimum(ns_acc)
-    I = Random.randperm(n)[1:nnz]
-    V = randn(nnz)
-    cs = zeros(n)
-    cs[I] .= V
-    rs, _, ws = test_case(:roots, [n], nothing, 1, nu=nu)
+            @printf(
+                    "Computing direct sum with ν = %i, n = %i (trial %i of %i)\n", 
+                    nu, n, t, n_trials
+                    )
+            gs_true[j,t] .= dir(nu, rs[I], cs[I], ws)
 
-    @printf(
-            "Computing direct sum with ν = %i, n = %i\n", 
-            nu, n
-            )
-    gs_true[j] .= dir(nu, rs[I], cs[I], ws)
+            for (i, tol) in enumerate(tols)
+                @printf(
+                    "Computing NUFHT with ν = %i, ε = %.0e, n = %i (%i of %i, trial %i of %i)\n", 
+                    nu, tol, n, i, length(tols), t, n_trials
+                    )
 
-    for (i, tol) in enumerate(tols)
-        @printf(
-            "Computing NUFHT with ν = %i, ε = %.0e, n = %i (%i of %i)\n", 
-            nu, tol, n, i, length(tols)
-            )
+                FastHankelTransform.setup_nufht!(nu, tol)
 
-        FastHankelTransform.setup_nufht!(nu, tol)
-
-        gs_nufht[j][i, :] = nufht(nu, rs, cs, ws, tol=tol)
+                gs_nufht[i,j,t] .= nufht(nu, rs, cs, ws, tol=tol)
+            end
+        end
     end
+    save(
+        "./accuracy.jld", 
+        "nu", nu,
+        "n_trials", n_trials,
+        "tols", tols,
+        "ns", ns_acc,
+        "gs_true", gs_true, 
+        "gs_nufht", gs_nufht
+        )
 end
 
 ## Plot accuracy as a function of tolerance
 
-max_errs = [maximum(abs.(gs_true[j] - gs_nufht[j][i,:])) / maximum(abs.(gs_true[j])) for j in eachindex(ns_acc), i in eachindex(tols)]
+max_errs_all = [maximum(abs.(gs_true[j,t] - gs_nufht[i,j,t])) / maximum(abs.(gs_true[j])) for j in eachindex(ns_acc), i in eachindex(tols), t=1:n_trials]
 
-l2_errs = [norm(gs_true[j] - gs_nufht[j][i,:]) / norm(gs_true[j]) for j in eachindex(ns_acc), i in eachindex(tols)]
+l2_errs_all = [norm(gs_true[j,t] - gs_nufht[i,j,t]) / norm(gs_true[j]) for j in eachindex(ns_acc), i in eachindex(tols), t=1:n_trials]
+
+##
+
+l2_errs  = reshape(sum(l2_errs_all,  dims=3) / n_trials, length(ns_acc), :)'
+max_errs = reshape(sum(max_errs_all, dims=3) / n_trials, length(ns_acc), :)'
 
 pl = plot(
-    tols, l2_errs',
+    tols, max_errs,
     line=(2, scrungle'), marker=(3, scrungle'), markerstrokewidth=0,
-    xlabel=L"$\varepsilon$", ylabel=L"rel. $\ell^2$ error",
+    xlabel=L"$\varepsilon$", 
+    # ylabel=L"rel. $\ell^2$ error",
+    ylabel="rel. max error",
     label=reshape([latexstring(@sprintf("\$n=10^{%i}\$", Int(log10(n)))) for n in ns_acc], 1, :),
     xscale=:log10, yscale=:log10, 
     ylims=(5e-16, 5e-4), xlims=(5e-16, 5e-4),
@@ -162,7 +222,7 @@ plot!([1e-16, 1e-2], [1e-16, 1e-2], line=(1, :black, :dash))
 display(pl)
 savefig(pl, "~/Downloads/accuracy.pdf")
 
-##
+## Plot n, m, and p scaling
 
 function plot_scaling(xs, ys, refs; xlabel="", ylims=[1e-2, 1e2], label="")
     pl = plot(
@@ -178,51 +238,54 @@ function plot_scaling(xs, ys, refs; xlabel="", ylims=[1e-2, 1e2], label="")
     return pl
 end
 
-cn = 0.8 * n_timings[1,end] / ns[end]
+cn = 0.6 * n_timings[2,end] / ns[end]
 n_refs = cn * ns
+n_refs[1:5] .= NaN
 pn = plot_scaling(
     ns, n_timings', n_refs, 
-    xlabel=L"$n$", ylims=[1e-2, 1e1], label=["NUFHT" "Direct"]
+    xlabel=L"$n$", ylims=[1e-2, 1e2], label=["Direct" "NUFHT"]
     )
 display(pn)
 savefig(pn, "~/Downloads/n_scaling.pdf")
 
-cm = 0.8 * m_timings[1,end] / ns[end]
+cm = 0.7 * m_timings[2,end] / ns[end]
 m_refs = cm * ns
-pm = plot_scaling(ns, m_timings', m_refs, xlabel=L"$m$", ylims=[1e-2, 1e1])
+m_refs[1:5] .= NaN
+pm = plot_scaling(ns, m_timings', m_refs, xlabel=L"$m$", ylims=[1e-2, 1e2])
 display(pm)
 savefig(pm, "~/Downloads/m_scaling.pdf")
 
-cp = 0.4 * p_timings[1,end] / (ps[end]*log(ps[end]))
+cp = 0.4 * p_timings[2,end] / (ps[end]*log(ps[end]))
 p_refs = cp * ps .* log.(ps)
+p_refs[1:5] .= NaN
 pp = plot_scaling(ps, p_timings', p_refs, xlabel=L"$p$", ylims=[1e-2, 1e2])
 savefig(pp, "~/Downloads/p_scaling.pdf")
 
-##
+## Plot both scaling
 
 pl = plot(
-    ns_both, hcat(roots_timings[1,:], exp_timings[1,:], exp_timings[2,:]), 
+    ns_both, hcat(roots_timings[2, 1, 2, :], exp_timings[2,:], roots_timings[1, 1, 2, :]), 
     line=(2, scrungle[[2,3,1]]'),
-    label=["NUFHT roots" "NUFHT exp" "Direct"],
+    label=["NUFHT F-B" "NUFHT exp" "Direct"],
     marker=(3, scrungle[[2,3,1]]'), markerstrokewidth=0,
     xlabel=L"$n$", ylabel="time (s)",
     xscale=:log10, yscale=:log10, 
-    ylims=[1e-3, 1e3], legend=:bottomright
+    ylims=[1e-3, 1e3], legend=:topleft
     )
 # n_refs = 0.7 * roots_timings[1,end] / ns_both[end] * ns_both
 # plot!(pl, ns_both[5:end], n_refs[5:end], line=(2, :black, :dash))
 
-nlogn_refs = 0.3 * exp_timings[1,end] / (ns_both[end]*log(ns_both[end])) * ns_both .* log.(ns_both)
+nlogn_refs = 0.05 * exp_timings[2,end] / (ns_both[end]*log(ns_both[end])) * ns_both .* log.(ns_both)
 plot!(pl, ns_both[4:end], nlogn_refs[4:end], line=(2, :black, :dash))
 
-n2_refs = 1.8 * exp_timings[2,4] / (ns_both[4]^2) * ns_both .^ 2
-plot!(pl, ns_both[3:5], n2_refs[3:5], line=(2, :black, :dashdot))
-
+n2_refs = 2.7 * exp_timings[1,4] / (ns_both[4]^2) * ns_both .^ 2
+plot!(pl, ns_both[2:4], n2_refs[2:4], line=(2, :black, :dashdot))
+display(pl)
 savefig(pl, "~/Downloads/both_scaling.pdf")
 
 ##
 
-colors = palette(["#FF8A8E", "#8AFFFB"], length(nus))[1:end]'
+colors = palette(["#FF8A8E", "#6eccc9"], length(nus))[1:end]'
 
 pl = plot(
     ns_both, roots_timings[2, :, 2, :]', 
@@ -234,15 +297,15 @@ pl = plot(
     ylims=[1e-3, 1e3], legend=:topleft
 )
 plot!(pl, 
-    ns_both, roots_timings[1, 1, 2, :], 
-    line=(2, :black), marker=(3, :black), label="Direct"
+    ns_both, roots_timings[1, 1, 2, :],  markerstrokewidth=0,
+    line=(2, scrungle[1]), marker=(3, scrungle[1]), label="Direct"
     )
 display(pl)
 savefig(pl, "~/Downloads/nu_scaling.pdf")
 
 ##
 
-colors = reshape(["#FFD1D3", palette(["#FF8A8E", "#660003"], length(tols)-1)[1:end]...], 1, :)
+colors = reshape(["#ffccce", palette(["#FF8A8E", "#800004"], length(tols)-1)[1:end]...], 1, :)
 
 pl = plot(
     ns_both, roots_timings[2, 1, :, :]', 
@@ -254,8 +317,8 @@ pl = plot(
     ylims=[1e-3, 1e3], legend=:topleft
 )
 plot!(pl, 
-    ns_both, roots_timings[1, 1, 2, :], 
-    line=(2, :black), marker=(3, :black), label="Direct"
+    ns_both, roots_timings[1, 1, 2, :],  markerstrokewidth=0,
+    line=(2, scrungle[1]), marker=(3, scrungle[1]), label="Direct"
     )
 display(pl)
 savefig(pl, "~/Downloads/tol_scaling.pdf")
