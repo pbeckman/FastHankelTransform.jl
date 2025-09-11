a20dfee59c9e4d898270b3a5664c5eb7ce76c675
Paul Beckman <paul.beckman@cims.nyu.edu>
2025-09-11 17:21:33 -0500

    uncomment figure generation

--- example/paper-figures/generate_figures.jl
+++ example/paper-figures/generate_figures.jl
@@ -19,17 +19,17 @@ scrungle = color.([
     "#DBDBDB" # grey
 ])
 
-# @printf("\n\nGENERATING FIGURE 1\n\n")
-# include("two_expansions_fig.jl")
+@printf("\n\nGENERATING FIGURE 1\n\n")
+include("two_expansions_fig.jl")
 
-# @printf("\n\nGENERATING FIGURE 2\n\n")
-# include("subdivide_fig.jl")
+@printf("\n\nGENERATING FIGURE 2\n\n")
+include("subdivide_fig.jl")
 
-# @printf("\n\nGENERATING FIGURES 3, 4, & 5 (this will take minutes to hours)\n\n")
-# include("performance_fig.jl")
+@printf("\n\nGENERATING FIGURES 3, 4, & 5 (this will take minutes to hours)\n\n")
+include("performance_fig.jl")
 
-# @printf("\n\nGENERATING FIGURE 6 (this will take minutes to hours)\n\n")
-# include("fourier_scaling_fig.jl")
+@printf("\n\nGENERATING FIGURE 6 (this will take minutes to hours)\n\n")
+include("fourier_scaling_fig.jl")
 
 @printf("\n\nGENERATING FIGURE 7 (this will take a few minutes)\n\n")
 include("fourier_bessel_fig.jl")
